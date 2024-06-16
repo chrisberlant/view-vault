ec6c88d2460f4664d20ab6967991799fee50e351
Chris Berlant <chris.berlant@gmail.com>
2024-06-17 00:03:42 +0200

    feat: landing page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { Button } from '../components/ui/button';
+import Link from 'next/link';
+import { buttonVariants } from '../components/ui/button';
 import Image from 'next/image';
 
 export default function Home() {
@@ -6,12 +7,22 @@ export default function Home() {
 		<main className='flex min-h-screen flex-col items-center p-12 gap-4'>
 			<h1 className='text-4xl mb-8'>Welcome to ViewVault</h1>
 			<Image src='/films.jpg' alt='' width={800} height={800} />
-			<div className='flex flex-col items-center gap-8 mt-8'>
-				<Button>Create an account</Button>
-				<div>
-					Already have an account?{' '}
-					<Button className='ml-4'>Sign in</Button>
-				</div>
+			<div className='flex flex-col items-center mt-8'>
+				<Link
+					href=''
+					className={buttonVariants({ variant: 'default' }) + ' mb-8'}
+				>
+					Create an account
+				</Link>
+				<Link
+					href=''
+					className={buttonVariants({ variant: 'default' }) + ' mb-2'}
+				>
+					Access without account
+				</Link>
+				<span className='text-sm'>
+					(You will not be able to save your favorite shows)
+				</span>
 			</div>
 		</main>
 	);
